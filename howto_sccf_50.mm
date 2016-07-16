fe38a9b2a552dd5422d315a6c09efead098dc9d5
a2kpi <reconf@free.fr>
2016-07-16 13:31:06 +0200

    Add files via upload

---
+++ howto_sccf_50.mm
@@ -0,0 +1,125 @@
+<map version="freeplane 1.5.9">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="R&#xe9;flexions&#xa;SCCF_50 Reconf 2.0" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1573594237" CREATED="1462710693327" MODIFIED="1468668585807" TEXT_ALIGN="CENTER" MAX_WIDTH="260.0 pt" MIN_WIDTH="182.0 pt" VGAP_QUANTITY="10.0 pt">
+<font SIZE="16"/>
+<hook NAME="MapStyle" background="#f7f7f7">
+    <properties show_icon_for_attributes="true" fit_to_viewport="false;" show_note_icons="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt" TEXT_SHORTENED="true">
+<font SIZE="24"/>
+<richcontent TYPE="DETAILS" LOCALIZED_HTML="styles_background_html"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<font SIZE="9"/>
+<stylenode LOCALIZED_TEXT="default" COLOR="#000000" STYLE="bubble" SHAPE_VERTICAL_MARGIN="0.0 pt" TEXT_ALIGN="CENTER" MAX_WIDTH="120.0 pt" MIN_WIDTH="120.0 pt">
+<font NAME="Arial" SIZE="9" BOLD="false" ITALIC="false"/>
+<edge STYLE="bezier" WIDTH="1"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes" COLOR="#000000" BACKGROUND_COLOR="#cccccc" MAX_WIDTH="260.0 pt" MIN_WIDTH="260.0 pt" VGAP_QUANTITY="5.0 pt">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<font SIZE="9"/>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+<edge COLOR="#0000cc"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<font SIZE="9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" STYLE="fork" UNIFORM_SHAPE="true" TEXT_ALIGN="CENTER" MAX_WIDTH="120.0 pt" MIN_WIDTH="100.0 pt" VGAP_QUANTITY="10.0 pt">
+<font SIZE="16"/>
+<edge STYLE="bezier" WIDTH="3"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" NUMBERED="true" MIN_WIDTH="100.0 pt" VGAP_QUANTITY="5.0 pt">
+<font SIZE="11" BOLD="true"/>
+<edge COLOR="#a3a3ff" WIDTH="1"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" MAX_WIDTH="200.0 pt" MIN_WIDTH="20.0 pt" VGAP_QUANTITY="5.0 pt" TEXT_ALIGN="LEFT">
+<edge COLOR="#ff77b3" WIDTH="1"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" MAX_WIDTH="260.0 pt" MIN_WIDTH="20.0 pt" TEXT_ALIGN="LEFT" VGAP_QUANTITY="5.0 pt">
+<edge COLOR="#ff9933" WIDTH="1"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" MAX_WIDTH="260.0 pt" MIN_WIDTH="20.0 pt" TEXT_ALIGN="LEFT" VGAP_QUANTITY="5.0 pt">
+<edge COLOR="#9999ff" WIDTH="1"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5">
+<edge COLOR="#968900"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6">
+<edge COLOR="#cc00cc"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7">
+<edge COLOR="#ffbf00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8">
+<edge COLOR="#00ff99"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9">
+<edge COLOR="#0099ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10">
+<edge COLOR="#996600"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11">
+<edge COLOR="#000000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,12">
+<edge COLOR="#cc0066"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,13">
+<edge COLOR="#33ff00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,14">
+<edge COLOR="#ff9999"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,15">
+<edge COLOR="#0000cc"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,16">
+<edge COLOR="#cccc00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,17">
+<edge COLOR="#0099cc"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,18">
+<edge COLOR="#006600"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,19">
+<edge COLOR="#ff00cc"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,20">
+<edge COLOR="#00cc00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,21">
+<edge COLOR="#0066cc"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,22">
+<edge COLOR="#00ffff"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="0" RULE="FOR_BRANCHES"/>
+<edge COLOR="#000000"/>
+<node TEXT="Intro &amp; contexte" LOCALIZED_STYLE_REF="AutomaticLayout.level,1" POSITION="left" ID="ID_1332007370" CREATED="1462729844988" MODIFIED="1468668585807" BACKGROUND_COLOR="#ffd5ad" VGAP_QUANTITY="5.0 pt" HGAP_QUANTITY="-196.0 pt" VSHIFT_QUANTITY="-37.5 pt">
+<hook NAME="FreeNode"/>
+<node TEXT="Cette carte de r&#xe9;flexions est un essai.&#xa;&#xa;Ses objectifs sont :" ID="ID_28841704" CREATED="1462715723956" MODIFIED="1468666458928" VGAP_QUANTITY="5.0 pt" HGAP_QUANTITY="-277.75 pt" VSHIFT_QUANTITY="-80.25 pt" TEXT_ALIGN="LEFT">
+<node TEXT="&lt;html&gt;&#xa;  &lt;head&gt;&#xa;    &#xa;  &lt;/head&gt;&#xa;  &lt;body&gt;&#xa;    &lt;p&gt;&#xa;      &lt;b style=&quot;background-color: yellow&quot;&gt;&lt;font color=&quot;black&quot;&gt;Bousculer&lt;/font&gt;&lt;/b&gt;&amp;#160;et &lt;b style=&quot;background-color: yellow&quot;&gt;r&amp;#233;veiller&lt;/b&gt;&amp;#160;les consciences anesth&amp;#233;si&amp;#233;es (synth&amp;#232;se pape Fran&amp;#231;ois)&#xa;    &lt;/p&gt;&#xa;  &lt;/body&gt;&#xa;&lt;/html&gt;" ID="ID_1804252477" CREATED="1468665842336" MODIFIED="1468666340371" HGAP_QUANTITY="-247.0 pt" VSHIFT_QUANTITY="-15.75 pt"/>
+<node TEXT="Organiser et classer r&#xe9;flexions et r&#xe9;f&#xe9;rences" ID="ID_11828535" CREATED="1468666040779" MODIFIED="1468666345358" HGAP_QUANTITY="-264.25 pt" VSHIFT_QUANTITY="-32.25 pt"/>
+<node TEXT="Questionner pour s&apos;approprier le projet national" ID="ID_463380917" CREATED="1468666073891" MODIFIED="1468666348459" HGAP_QUANTITY="-272.5 pt" VSHIFT_QUANTITY="-24.0 pt"/>
+<node TEXT="Proposer des pistes simples et rapide &#xe0; d&#xe9;ployer" ID="ID_564268273" CREATED="1468666089556" MODIFIED="1468666452592" HGAP_QUANTITY="-268.75 pt" VSHIFT_QUANTITY="-3.0 pt" TEXT_ALIGN="DEFAULT"/>
+</node>
+</node>
+</node>
+</map>
