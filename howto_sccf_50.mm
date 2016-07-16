925226ba611d42378de25ec171f02da9cd7bac83
a2kpi <reconf@free.fr>
2016-07-16 21:10:38 +0200

    majour intro et image

--- howto_sccf_50.mm
+++ howto_sccf_50.mm
@@ -1,6 +1,7 @@
 <map version="freeplane 1.5.9">
 <!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
-<node TEXT="R&#xc9;FLEXIONS&#xa;&#xa;Dynamiser le SCCF_50&#xa;S&apos;approprier le projet national" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1573594237" CREATED="1462710693327" MODIFIED="1468682849441" BACKGROUND_COLOR="#33ccff" TEXT_ALIGN="CENTER" MAX_WIDTH="260.0 pt" MIN_WIDTH="182.0 pt" VGAP_QUANTITY="10.0 pt">
+<node TEXT="R&#xc9;FLEXIONS&#xa;&#xa;Dynamiser le SCCF_50&#xa;S&apos;approprier le projet national" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1573594237" CREATED="1462710693327" MODIFIED="1468696157508" BACKGROUND_COLOR="#33ccff" STYLE="bubble" TEXT_ALIGN="CENTER" MAX_WIDTH="260.0 pt" MIN_WIDTH="182.0 pt" VGAP_QUANTITY="10.0 pt">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1800473712" MIDDLE_LABEL="R&#xe9;veillons-nous !!!" STARTINCLINATION="-47;-57;" ENDINCLINATION="29;82;" STARTARROW="NONE" ENDARROW="NONE"/>
 <font SIZE="16"/>
 <hook NAME="MapStyle" background="#f7f7f7">
     <properties fit_to_viewport="false;" show_icon_for_attributes="true" show_note_icons="true" show_notes_in_map="true"/>
@@ -112,9 +113,7 @@
 </hook>
 <hook NAME="AutomaticEdgeColor" COUNTER="0" RULE="FOR_BRANCHES"/>
 <edge COLOR="#000000"/>
-<node LOCALIZED_STYLE_REF="defaultstyle.floating" POSITION="right" ID="ID_1800473712" CREATED="1468682069958" MODIFIED="1468682849436" HGAP_QUANTITY="-57.75 pt" VSHIFT_QUANTITY="-97.5 pt" MAX_WIDTH="15.0 cm" MIN_WIDTH="11.0 cm" STYLE="bubble" SHAPE_VERTICAL_MARGIN="0.5 cm">
-<icon BUILTIN="ksmiletris"/>
-<richcontent TYPE="NODE">
+<node LOCALIZED_STYLE_REF="defaultstyle.floating" POSITION="right" ID="ID_1800473712" CREATED="1468682069958" MODIFIED="1468696137230" HGAP_QUANTITY="-49.5 pt" VSHIFT_QUANTITY="-185.25 pt" MAX_WIDTH="15.0 cm" MIN_WIDTH="11.0 cm" BACKGROUND_COLOR="#ffff99" STYLE="bubble" SHAPE_VERTICAL_MARGIN="0.5 cm"><richcontent TYPE="NODE">
 
 <html>
   <head>
@@ -134,7 +133,13 @@
       
     </p>
     <p>
-      et refuser le confort du &quot;ah bin &#231;a, on le fait d&#233;j&#224;&quot; !...
+      et <b>refuser le confort</b>&#160;du &quot;ah bin &#231;a, on le fait d&#233;j&#224;&quot; !...
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      ...Pour <b><font size="3">&#233;viter la chronique d'une mort annonc&#233;e</font></b>&#160;!!!
     </p>
   </body>
 </html>
@@ -142,8 +147,8 @@
 </richcontent>
 <hook NAME="FreeNode"/>
 </node>
-<node TEXT="Introduction et contexte" LOCALIZED_STYLE_REF="AutomaticLayout.level,1" POSITION="right" ID="ID_1118780235" CREATED="1462729792536" MODIFIED="1468682634243" BACKGROUND_COLOR="#ccffcc" HGAP_QUANTITY="92.0 pt" VSHIFT_QUANTITY="-26.25 pt">
-<node TEXT="Cette carte mentale est un essai.&#xa;&#xa;Ses objectifs sont :" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="ID_1001148534" CREATED="1468679307179" MODIFIED="1468679371974">
+<node TEXT="Introduction et contexte" LOCALIZED_STYLE_REF="AutomaticLayout.level,1" POSITION="right" ID="ID_1118780235" CREATED="1462729792536" MODIFIED="1468696153146" BACKGROUND_COLOR="#ccffcc" VGAP_QUANTITY="5.0 pt" HGAP_QUANTITY="92.0 pt" VSHIFT_QUANTITY="-26.25 pt">
+<node TEXT="Cette carte mentale est un essai.&#xa;&#xa;Ses objectifs sont :" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="ID_1001148534" CREATED="1468679307179" MODIFIED="1468694041166">
 <node TEXT="&lt;html&gt;&#xa;  &lt;head&gt;&#xa;    &#xa;  &lt;/head&gt;&#xa;  &lt;body&gt;&#xa;    &lt;p&gt;&#xa;      &lt;b&gt;&lt;font color=&quot;#000000&quot;&gt;Bousculer&lt;/font&gt;&lt;/b&gt;&amp;#160;et &lt;b&gt;r&amp;#233;veiller&lt;/b&gt;&amp;#160;les consciences&#xa;    &lt;/p&gt;&#xa;  &lt;/body&gt;&#xa;&lt;/html&gt;" ID="ID_521513880" CREATED="1468679392141" MODIFIED="1468690402664">
 <node ID="ID_499854772" CREATED="1468689828393" MODIFIED="1468690458292" MAX_WIDTH="10.0 cm" MIN_WIDTH="5.0 cm" TEXT_ALIGN="LEFT"><richcontent TYPE="NODE">
 
@@ -230,6 +235,51 @@
 </richcontent>
 </node>
 </node>
+<node LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="ID_927732308" CREATED="1468691831890" MODIFIED="1468696153145" HGAP_QUANTITY="37.25 pt" VSHIFT_QUANTITY="27.75 pt"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>Que fait la concurrence</b>&#160;dans le domaine du caritatif ?
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_338420534" MIDDLE_LABEL="Attention..." STARTINCLINATION="-7;32;" ENDINCLINATION="48;-49;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<node TEXT="Le grand fr&#xe8;re protestant" ID="ID_1451455332" CREATED="1468693328627" MODIFIED="1468693339234"/>
+<node TEXT="La fondation EMMA&#xdc;S" ID="ID_594347152" CREATED="1468693348420" MODIFIED="1468693364524"/>
+<node TEXT="Les assos d&apos;insertion" ID="ID_41889639" CREATED="1468693416526" MODIFIED="1468693425269"/>
+<node TEXT="Les Restos du Coeur" ID="ID_1594396953" CREATED="1468693433791" MODIFIED="1468693448557"/>
+</node>
+<node LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="ID_326826058" CREATED="1468693538282" MODIFIED="1468695228714" HGAP_QUANTITY="20.75 pt" VSHIFT_QUANTITY="140.25 pt"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>Quels sont nos leviers</b>&#160;d'action au sein du SCCF ?
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1050692940" MIDDLE_LABEL="Et pourtant..." STARTINCLINATION="-64;14;" ENDINCLINATION="29;-23;" STARTARROW="NONE" ENDARROW="NONE"/>
+</node>
+</node>
+<node TEXT="...La concurrence est **TR&#xc8;S** rude !!!&#xa;&#xa;Les b&#xe9;n&#xe9;voles &quot;New Gen&quot; se tournent vers des assos dynamiques &#xe0; la pointe des techniques num&#xe9;riques" LOCALIZED_STYLE_REF="defaultstyle.floating" POSITION="right" ID="ID_338420534" CREATED="1468692763138" MODIFIED="1468696154979" HGAP_QUANTITY="489.0 pt" VSHIFT_QUANTITY="178.5 pt" MAX_WIDTH="250.0 pt" MIN_WIDTH="250.0 pt" BACKGROUND_COLOR="#ffff99">
+<icon BUILTIN="messagebox_warning"/>
+<hook NAME="FreeNode"/>
+<edge STYLE="hide_edge" WIDTH="4"/>
+</node>
+<node TEXT="...Nous disposons de tous les outils pour dynamiser le SCCF_50 et son territoire !!!" LOCALIZED_STYLE_REF="defaultstyle.floating" POSITION="right" ID="ID_1050692940" CREATED="1468693631430" MODIFIED="1468696157506" HGAP_QUANTITY="480.0 pt" VSHIFT_QUANTITY="362.25 pt" MAX_WIDTH="6.0 cm" MIN_WIDTH="6.0 cm" BACKGROUND_COLOR="#ffff99">
+<icon BUILTIN="bookmark"/>
+<hook NAME="FreeNode"/>
 </node>
 </node>
 </map>
